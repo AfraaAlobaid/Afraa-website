b2a6b408f818a3dd925be6bf8bea06a28bc2325a
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-03-21 23:08:53 +1030

    add html (basic structure)

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+function refreshPage(){
+    //document.getElementById("main").style.visibility = "hidden";
+    window.location.reload();
+}
+
+function showEdu(){
+    //empty the hello message
+    document.getElementById("main").innerHTML = "";
+
+    //create box for education element
+    let uniAdelaide = document.createElement("div");
+    uniAdelaide.className = "edu";
+    document.getElementById("main").appendChild(uniAdelaide);
+    // insert uni logo
+    let uniLogo = document.createElement("img");
+    uniLogo.src = "./media/uniAdelaideLogo.gif";
+    uniLogo.id = "uniLogo";
+    uniAdelaide.appendChild(uniLogo);
+
+    //insert text
+    let uniAdText = document.createElement("p");
+    uniAdText.innerHTML = "University of Adelaide";
+    
+
+
+}
