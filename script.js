4bec43172c1e335e58f3e9841a7dbaf9c981f86a
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-03-22 20:22:56 +1030

    add css

--- script.js
+++ script.js
@@ -18,9 +18,27 @@ function showEdu(){
     uniAdelaide.appendChild(uniLogo);
 
     //insert text
-    let uniAdText = document.createElement("p");
-    uniAdText.innerHTML = "University of Adelaide";
+    let uniAdText = document.createElement("div");
+    uniAdText.innerHTML = "<p>University of Adelaide</p> <p>Master of Software Engineering - ongoing</p> <p>2020 - 2021</p>";
+    uniAdelaide.appendChild(uniAdText);
+}
+
+function skill(){
+    //empty the main to place new elements
+    document.getElementById("main").innerHTML = "";
+    //create a new div for skill lists 
+    let skillDiv = document.createElement("div");
+    skillDiv.id = "skillDiv";
+    document.getElementById("main").appendChild(skillDiv);
+    //create a list of object oriented programming skills
+    let objOreinted = document.createElement("div");
+    objOreinted.innerHTML = "<h6>Object Oreinted Languages</h6> <ul><li>Java<li>Python<li>C++<li>C#<li></ul>"
+    skillDiv.appendChild(objOreinted);
+
+    //create a list of web development skills
+    let webSkills = document.createElement("ul");
+
+
+
     
-
-
 }
